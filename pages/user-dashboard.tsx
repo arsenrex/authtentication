bea77b07ab9ea408148b7d7f22142b2bfc23ef01
Arsen Sarumaha <103574696+arsenrex@users.noreply.github.com>
2024-11-21 01:58:56 +0700

    Create user-dashboard.tsx

---
+++ pages/user-dashboard.tsx
@@ -0,0 +1,8 @@
+export default function UserDashboard() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl mb-4">User Dashboard</h1>
+      <p>Selamat datang, User!</p>
+    </div>
+  );
+}
